10a163f4de5156d4fdcc1d1f80f4c6b2e15a25a0
Katja Durrani <mail@kdurrani.co.uk>
2018-10-15 19:06:57 +0100

    update test_views in books app to use client and admin_client fixtures, alternatively use CustomUser model

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -1,17 +1,22 @@
 import pytest
 from django.test import RequestFactory
 from django.contrib.auth.models import AnonymousUser
-from mixer.backend.django import mixer
+from django.contrib.auth import get_user_model
+from books import views
+
+CustomUser = get_user_model()
 
 pytestmark = pytest.mark.django_db
 
-from .. import views
 
 class TestHomePageView:
     def test_anonymous(self):
         req = RequestFactory().get('/')
         resp = views.HomePageView.as_view()(req)
         assert resp.status_code == 200, 'Should be callable by anyone'
+
+# for now leaving in both RequestFactory and Client 
+# for comparison
 
 class TestBookNewView:
     def test_anonymous(self):
@@ -20,12 +25,24 @@ class TestBookNewView:
         resp = views.BookNewView.as_view()(req)
         assert 'login' in resp.url, 'Should not allow access to anonymous'
 
-# did not manage to import CustomUser or any other
-# authenticated User model so far :/
+    def test_loggedin_user(self):        
+        req = RequestFactory().get('/')
+        req.user = CustomUser()
+        resp = views.BookNewView.as_view()(req)
+        assert resp.status_code == 200, 'Authenticated user can access'
 
-# def test_loggedin_user(self):
-#         user = mixer.blend('CustomUser', is_superuser=True)
-#         req = RequestFactory().get('/')
-#         req.user = user
-#         resp = views.BookNewView.as_view()(req)
-#         assert resp.status_code == 200, 'Authenticated user can access'
+    def test_with_unauth_client(self, client):
+        resp = client.get('/book/new')
+        assert 'login' in resp.url
+
+    def test_with_auth_client(self, client):
+        username = "user1"
+        password = "hiya"
+        CustomUser.objects.create_user(username=username, password=password)
+        client.login(username=username, password=password)
+        resp = client.get('/book/new')
+        assert resp.status_code == 200
+
+    def test_with_auth_client(self, admin_client):
+        resp = admin_client.get('/book/new')
+        assert resp.status_code == 200
