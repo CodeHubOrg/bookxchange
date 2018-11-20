14ba3670ff9cc3d9d80e15bd7c5c175519dd6119
Katja Durrani <mail@kdurrani.co.uk>
2018-11-20 19:13:34 +0000

    resizes image on saving book, plus create thumbnail

--- books/models.py
+++ books/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.conf import settings
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage as storage
+
+from PIL import Image, ImageOps
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
+import os
+
+
 
 default_owner_name = "Codehub"
 def get_default_owner():
@@ -10,6 +22,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     cover = models.ImageField(upload_to='covers/', blank=True)
+    thumb = models.ImageField(upload_to='covers/', blank=True)
     published_date = models.DateTimeField(blank=True, null=True)
     last_updated = models.DateTimeField(auto_now_add=True, null=True)
     owner = models.ForeignKey(get_user_model(), on_delete=models.SET(get_default_owner))
@@ -36,6 +49,65 @@ class Book(models.Model):
     @property
     def delete_url(self):
         return reverse('book_delete', kwargs={'pk':self.pk})
+
+    def save(self):
+        # print(self.cover)
+        if self.pk is None:
+
+
+            
+            if not self.make_thumbnail():
+                # set to a default thumbnail
+                raise Exception('Could not create thumbnail - is the file type valid?')
+
+            
+            im = Image.open(self.cover)
+            imagefit = ImageOps.fit(im, (200,300), Image.ANTIALIAS)
+         
+
+            output = BytesIO()            
+            imagefit.save(output, format='JPEG', quality=90)
+            output.seek(0)    
+            self.cover = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.cover.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
+
+
+            # self.make_thumbnail()
+
+            if not self.make_thumbnail():
+                # set to a default thumbnail
+                raise Exception('Could not create thumbnail - is the file type valid?')
+
+            
+        super(Book,self).save()
+
+
+    def make_thumbnail(self):
+
+        image = Image.open(self.cover)
+        image.thumbnail((100,100), Image.ANTIALIAS)
+
+        thumb_name, thumb_extension = os.path.splitext(self.cover.name)
+        thumb_extension = thumb_extension.lower()
+
+        thumb_filename = thumb_name + '_thumb' + thumb_extension
+        if thumb_extension in ['.jpg', '.jpeg']:
+            FTYPE = 'JPEG'
+        elif thumb_extension == '.gif':
+            FTYPE = 'GIF'
+        elif thumb_extension == '.png':
+            FTYPE = 'PNG'
+        else:
+            return False 
+
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, FTYPE)
+        temp_thumb.seek(0)
+
+        self.thumb.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+        temp_thumb.close()
+
+        return True
+    
 
     def __str__(self):
         return self.title
