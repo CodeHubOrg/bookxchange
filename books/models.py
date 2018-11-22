a1712b30e2e6237a5dcfda677b2c1f100c775fbf
Katja Durrani <mail@kdurrani.co.uk>
2018-11-22 17:10:23 +0000

    use linting tool and make recommended changes to functions and error handling

--- books/models.py
+++ books/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
 from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage as storage
-
 from PIL import Image, ImageOps
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
@@ -13,10 +10,12 @@ import sys
 import os
 
 
-
 default_owner_name = "Codehub"
+
+
 def get_default_owner():
     return get_user_model().objects.get_or_create(username=default_owner_name)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -25,7 +24,8 @@ class Book(models.Model):
     thumb = models.ImageField(upload_to='covers/', blank=True)
     published_date = models.DateTimeField(blank=True, null=True)
     last_updated = models.DateTimeField(auto_now_add=True, null=True)
-    owner = models.ForeignKey(get_user_model(), on_delete=models.SET(get_default_owner))
+    owner = models.ForeignKey(get_user_model(),
+                              on_delete=models.SET(get_default_owner))
 
     @property
     def display_author(self):
@@ -36,67 +36,77 @@ class Book(models.Model):
         return "".join(lastfirst)
 
     def get_absolute_url(self):
-        return reverse('book_detail', kwargs={'pk':self.pk})
-    
+        return reverse('book_detail', kwargs={'pk': self.pk})
+
     @property
     def absolute_url(self):
-        return self.get_absolute_url()    
+        return self.get_absolute_url()
 
     @property
     def update_url(self):
-        return reverse('book_update', kwargs={'pk':self.pk})
-    
+        return reverse('book_update', kwargs={'pk': self.pk})
+
     @property
     def delete_url(self):
-        return reverse('book_delete', kwargs={'pk':self.pk})
+        return reverse('book_delete', kwargs={'pk': self.pk})
 
-    def save(self):                    
+    def save(self):
+        if(self.cover):
+            name, extension = os.path.splitext(self.cover.name)
+            extension = extension.lower()
+            thumb_filename = name + '_thumb' + extension
+
+            self.resize_image(200, 300, extension, 90)
+            self.make_thumbnail(thumb_filename, extension)
+
+        super(Book, self).save()
+
+    def resize_image(self, width, height, ext, quality):
         im = Image.open(self.cover)
-        imagefit = ImageOps.fit(im, (200,300), Image.ANTIALIAS)
+        imagefit = ImageOps.fit(im, (width, height), Image.ANTIALIAS)
+        ftype = self.get_file_extension(ext)
 
-        name, extension = os.path.splitext(self.cover.name)
-        extension = extension.lower()
-        thumb_filename = name + '_thumb' + extension
-        FTYPE = self.getFileExtension(extension)
-
-        output = BytesIO()            
-        imagefit.save(output, format=FTYPE, quality=90)
-        output.seek(0)    
-        self.cover = InMemoryUploadedFile(output,'ImageField', "{0}.{1}".format(self.cover.name.split('.')[0], extension), "image/%s" %extension, sys.getsizeof(output), None)
-
-       
-        if not self.make_thumbnail(thumb_filename, extension):
-            raise Exception('Could not create thumbnail - is the file type valid?')
-            
-        super(Book,self).save()
-
+        output = BytesIO()
+        imagefit.save(output, format=ftype, quality=90)
+        output.seek(0)
+        self.cover = InMemoryUploadedFile(
+                output, 'ImageField',
+                "{0}.{1}".format(
+                    self.cover.name.split('.')[0], ext),
+                "image/%s" % ext,
+                sys.getsizeof(output), None)
 
     def make_thumbnail(self, name, ext):
         image = Image.open(self.cover)
-        image.thumbnail((100,100), Image.ANTIALIAS)
+        image.thumbnail((100, 100), Image.ANTIALIAS)
 
         temp_thumb = BytesIO()
-        FTYPETHUMB = self.getFileExtension(ext)
-        image.save(temp_thumb, FTYPETHUMB)
+        ftype = self.get_file_extension(ext)
+        image.save(temp_thumb, ftype)
         temp_thumb.seek(0)
 
         self.thumb.save(name, ContentFile(temp_thumb.read()), save=False)
         temp_thumb.close()
 
-        return True
+    def get_file_extension(self, extension):
+        ext_to_type = {
+            '.jpg': 'JPEG',
+            '.jpeg': 'JPEG',
+            '.png': 'PNG',
+            '.gif': 'GIF'
+        }
 
-    def getFileExtension(self, extension):
-        if extension in ['.jpg', '.jpeg']:
-            ext = 'JPEG'
-        elif extension == '.gif':
-            ext = 'GIF'
-        elif extension == '.png':
-            ext = 'PNG'
-        else:
-            return False
-        return ext
-
-    
+        try:
+            return ext_to_type[extension.lower()]
+        except KeyError:
+            valid_extensions = ", ".join(ext_to_type.keys())
+            raise InvalidExtension(
+                f"Could not recognise file extension {extension}. \
+                Supported extensions: {valid_extensions}")
 
     def __str__(self):
         return self.title
+
+
+class InvalidExtension(Exception):
+    """Raise for invalid image extension"""
