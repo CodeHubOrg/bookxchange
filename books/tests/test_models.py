afd3554803da0d6cac885ed4576c301a3acc6523
Katja Durrani <mail@kdurrani.co.uk>
2018-12-03 19:12:30 +0000

    test travis

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -1,7 +1,7 @@
 import pytest
 from mixer.backend.django import mixer
 
-# from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 
 
 class TestBook:
@@ -11,13 +11,13 @@ class TestBook:
         assert book.__str__() == "A book", "Should return title"
 
 
-# class TestBookHolder:
-#     @pytest.mark.django_db
-#     def test_model(self):
-#         book = mixer.blend("books.Book", title="A book")
-#         user1 = mixer.blend(
-#             get_user_model(), username="user1", email="mail@example.com"
-#         )
-#         book_held = mixer.blend("books.BookHolder", book=book, holder=user1)
-#         assert book_held.date_requested is None
-#         assert book_held.holder == user1
+class TestBookHolder:
+    @pytest.mark.django_db
+    def test_model(self):
+        book = mixer.blend("books.Book", title="A book")
+        user1 = mixer.blend(
+            get_user_model(), username="user1", email="mail@example.com"
+        )
+        book_held = mixer.blend("books.BookHolder", book=book, holder=user1)
+        assert book_held.date_requested is None
+        assert book_held.holder == user1
