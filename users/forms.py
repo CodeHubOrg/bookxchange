fad07c21f21e7b01206c31a79e16d7f566e98799
Katja Durrani <mail@kdurrani.co.uk>
2019-01-22 02:04:33 +0000

    style all the forms

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,30 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text="Required")
+    email = forms.EmailField(
+        max_length=200, widget=forms.EmailInput(attrs={"class": "uk-input"})
+    )
+    password1 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"class": "uk-input"}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=("Password confirmation"),
+        widget=forms.PasswordInput(attrs={"class": "uk-input"}),
+        strip=False,
+        help_text=("Enter the same password as before, for verification."),
+    )
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ("username", "email", "password1", "password2")
+        fields = ("email", "username", "password1", "password2")
+        widgets = {"username": forms.TextInput(attrs={"class": "uk-input"})}
 
 
 class CustomUserChangeForm(UserChangeForm):
